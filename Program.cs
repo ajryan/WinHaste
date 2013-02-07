549964c039c31d2908667b37817f593b271fb827
Aidan Ryan <ryan.aidan@gmail.com>
2013-02-07 12:55:16 -0800

    Echo piped input back to console

--- Program.cs
+++ Program.cs
@@ -6,57 +6,79 @@ using System.Windows.Forms;
 
 namespace WinHaste
 {
-	class Program
-	{
-		private static readonly Regex _HasteKeyRegex = new Regex(@"{""key"":""(?<key>[a-z].*)""}", RegexOptions.Compiled);
+    class Program
+    {
+        private static readonly Regex _HasteKeyRegex = new Regex(@"{""key"":""(?<key>[a-z].*)""}", RegexOptions.Compiled);
 
-		[STAThread]
-		static void Main(string[] args)
-		{
-			var parameters = new Parameters(args);
-			var parseResult = parameters.Parse();
-			if (parseResult != Parameters.ParseResult.Success)
-			{
-				Console.WriteLine(parameters.Usage);
-				Console.WriteLine(Environment.NewLine + "ERROR: " + parseResult);
-				return;
-			}
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var parameters = new Parameters(args);
+            var parseResult = parameters.Parse();
+            if (parseResult != Parameters.ParseResult.Success)
+            {
+                Console.WriteLine(parameters.Usage);
+                Console.WriteLine(Environment.NewLine + "ERROR: " + parseResult);
+                return;
+            }
 
-			string haste;
-			if (!String.IsNullOrEmpty(parameters.Filename))
-			{
-				haste = File.ReadAllText(parameters.Filename);
-			}
-			else
-			{
-				haste = String.Empty;
+            string haste;
+            if (!String.IsNullOrEmpty(parameters.Filename))
+            {
+                haste = File.ReadAllText(parameters.Filename);
+            }
+            else
+            {
+                bool piped = IsInputPiped();
 
-				int consoleKey = Console.Read();
-				while (consoleKey != -1)
-				{
-					var consoleChar = Convert.ToChar(consoleKey);
-					haste += consoleChar;
+                haste = String.Empty;
 
-					consoleKey = Console.Read();
-				}
-			}
+                int consoleKey = Console.Read();
+                while (consoleKey != -1)
+                {
+                    var consoleChar = Convert.ToChar(consoleKey);
 
-			using (var client = new WebClient())
-			{
-				var response = client.UploadString(parameters.Url + "/documents", haste);
-				var match = _HasteKeyRegex.Match(response);
+                    if (piped)
+                    {
+                        Console.Write(consoleChar);
+                        Console.Out.Flush();
+                    }
 
-				if (!match.Success)
-				{
-					Console.WriteLine(response);
-					return;
-				}
+                    haste += consoleChar;
 
-				string hasteUrl = "http://hastebin.com/" + match.Groups["key"];
+                    consoleKey = Console.Read();
+                }
+            }
 
-				Clipboard.SetText(hasteUrl);
-				Console.WriteLine("\r\nHaste URL: {0}  (copied to clipboard)", hasteUrl);
-			}
-		}
-	}
+            using (var client = new WebClient())
+            {
+                var response = client.UploadString(parameters.Url + "/documents", haste);
+                var match = _HasteKeyRegex.Match(response);
+
+                if (!match.Success)
+                {
+                    Console.WriteLine(response);
+                    return;
+                }
+
+                string hasteUrl = "http://hastebin.com/" + match.Groups["key"];
+
+                Clipboard.SetText(hasteUrl);
+                Console.WriteLine("\r\nHaste URL: {0}  (copied to clipboard)", hasteUrl);
+            }
+        }
+
+        private static bool IsInputPiped()
+        {
+            try
+            {
+                var tmp = Console.KeyAvailable;
+                return false;
+            }
+            catch (InvalidOperationException)
+            {
+                return true;
+            }
+        }
+    }
 }
