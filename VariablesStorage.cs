9a1ef0959066b0ae6b4007ec5b3a987498dbec55
Emil Gusev <lunnaholy@mail.ru>
2022-11-18 03:29:25 +0500

    Support for changing the default baseUrl

--- VariablesStorage.cs
+++ VariablesStorage.cs
@@ -6,18 +6,26 @@ namespace WinHaste
 {
     class VariablesStorage
     {
-        private static string _configPath = "%UserName%/.winhaste/config.json";
+        private static string _directoryPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "/.winhaste";
+        private static string _configPath = _directoryPath + "/config.ini";
         private static string _defaultConfig = "baseUrl=https://hastebin.com";
 
         public static bool IsDirectoryExists()
         {
+            if (!Directory.Exists(_directoryPath))
+            {
+                Directory.CreateDirectory(_directoryPath);
+            }
             return File.Exists(_configPath);
         }
 
         public static void InitConfigFile()
         {
             if (IsDirectoryExists()) return;
-            File.WriteAllText(_configPath, _defaultConfig);
+            using (StreamWriter fs = File.CreateText(_configPath))
+            {
+                fs.Write(_defaultConfig);
+            }
         }
 
         public static Dictionary<String, String> GetConfig()
